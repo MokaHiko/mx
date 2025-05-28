d90dd81af4a59fd14b09be19a80776785daecc6b
Christian Mark G. Solon <christianmarkg.solon@192.168.1.4>
2025-05-28 08:46:11 +0800

    Updated hash map api.

--- include/mx/mx_hash.h
+++ include/mx/mx_hash.h
@@ -6,42 +6,45 @@
 
 MX_API void mx_murmur_hash_32(const void* key, int len, uint32_t seed, uint32_t* out);
 
-#define mx_safe_cast(ptr, value_t) = value_t(ptr)
-
-typedef void(*mx_map_add_fn(int));
-
-// Map entry struct generator with unique name
-#define mx_map_t(key_t, value_t, elem_count)                                                       \
-    typedef struct {                                                                               \
-        key_t key;                                                                                 \
-        value_t value;                                                                             \
-        UT_hash_handle hh;                                                                         \
-    } mx_concat(mx_concat(key_t, value_t), _map_entry);                                            \
-    typedef struct {                                                                               \
-        mx_concat(mx_concat(key_t, value_t), _map_entry) entries[elem_count];                      \
-        mx_concat(mx_concat(key_t, value_t), _map_entry) * head;                                   \
-        uint32_t entry_count;                                                                      \
-    } mx_concat(mx_concat(key_t, value_t), _map);                                                  \
-    MX_NO_DISCARD static inline value_t* mx_concat(mx_concat(mx_concat(key_t, value_t), _map),     \
-                                                   _find)(                                         \
-        mx_concat(mx_concat(key_t, value_t), _map) * mx_map_ptr, key_t key) {                      \
-        mx_concat(mx_concat(key_t, value_t), _map_entry) * entry;                                  \
-        HASH_FIND(hh, mx_map_ptr->head, &key, sizeof(key), entry);                                 \
-        return entry ? &entry->value : NULL;                                                       \
-    };                                                                                             \
-    static inline void mx_concat(mx_concat(mx_concat(key_t, value_t), _map),                       \
-                                 _insert)(mx_concat(mx_concat(key_t, value_t), _map) * mx_map_ptr, \
-                                          key_t key,                                               \
-                                          const value_t* value_ptr) {                              \
-        memcpy(&mx_map_ptr->entries[mx_map_ptr->entry_count].value, value_ptr, sizeof(value_t));   \
-        mx_map_ptr->entries[mx_map_ptr->entry_count].key = key;                                    \
-        HASH_ADD(hh,                                                                               \
-                 mx_map_ptr->head,                                                                 \
-                 key,                                                                              \
-                 sizeof(key),                                                                      \
-                 &mx_map_ptr->entries[mx_map_ptr->entry_count]);                                   \
-        ++mx_map_ptr->entry_count;                                                                 \
-    };                                                                                             \
+#define mx_map_t(key_t, value_t, elem_count)                                             \
+    typedef struct {                                                                     \
+        key_t key;                                                                       \
+        value_t value;                                                                   \
+        UT_hash_handle hh;                                                               \
+    } mx_concat(mx_concat(key_t, value_t), _map_entry);                                  \
+    typedef struct {                                                                     \
+        mx_concat(mx_concat(key_t, value_t), _map_entry) entries[elem_count];            \
+        mx_concat(mx_concat(key_t, value_t), _map_entry) * head;                         \
+        uint32_t entry_count;                                                            \
+    } mx_concat(mx_concat(key_t, value_t), _map);                                        \
     mx_concat(mx_concat(key_t, value_t), _map)
+
+#define mx_map_find_fn(fn_name, key_t, value_t)                                          \
+    MX_NO_DISCARD static inline value_t* fn_name(                                        \
+        mx_concat(mx_concat(key_t, value_t), _map) * mx_map_ptr,                         \
+        key_t key) {                                                                     \
+        mx_concat(mx_concat(key_t, value_t), _map_entry) * entry;                        \
+        HASH_FIND(hh, mx_map_ptr->head, &key, sizeof(key), entry);                       \
+        return entry ? &entry->value : NULL;                                             \
+    };
+
+#define mx_map_insert_fn(fn_name, key_t, value_t)                                        \
+    static inline void fn_name(mx_concat(mx_concat(key_t, value_t), _map) * mx_map_ptr,  \
+                               key_t key,                                                \
+                               const value_t* value_ptr) {                               \
+        MX_ASSERT(mx_map_ptr->entry_count <                                              \
+                  sizeof(mx_map_ptr->entries) /                                          \
+                      sizeof(mx_concat(mx_concat(key_t, value_t), _map_entry)));         \
+        memcpy(&mx_map_ptr->entries[mx_map_ptr->entry_count].value,                      \
+               value_ptr,                                                                \
+               sizeof(value_t));                                                         \
+        mx_map_ptr->entries[mx_map_ptr->entry_count].key = key;                          \
+        HASH_ADD(hh,                                                                     \
+                 mx_map_ptr->head,                                                       \
+                 key,                                                                    \
+                 sizeof(key),                                                            \
+                 &mx_map_ptr->entries[mx_map_ptr->entry_count]);                         \
+        ++mx_map_ptr->entry_count;                                                       \
+    };
 
 #endif
